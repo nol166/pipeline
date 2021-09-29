f93fd255ad4d1c4b6728b361668b82be3a297c8d
John McCambridge <jmccamb@gmail.com>
2021-09-28 22:00:32 -0500

    Remove static route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,6 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-// serve static html files
-router.use(express.static('public'));
-
 router.use((req, res) => {
   return res.send('Wrong route!');
 });
