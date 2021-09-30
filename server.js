d25230dea17bd90e5bd2dc793dfbaeda13654b3a
John McCambridge <jmccamb@gmail.com>
2021-09-30 11:39:05 -0500

    Update port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const { client } = require('./db/connection');
+const port = process.env.PORT || 3000;
 
 client.connect((err) =>
   err ? console.log(err) : console.log('Connected to MongoDB Atlas 🚀')
@@ -10,5 +11,5 @@ client.connect((err) =>
 client.on('open', () => {
   app.use(express.json());
   app.use(routes);
-  app.listen(3000, () => console.log('Server running on port 3000 🚀'));
+  app.listen(port, () => console.log('Server running on port 3000 🚀'));
 });
